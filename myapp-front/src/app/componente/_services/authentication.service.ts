76f0d500fe217a8d1c81a613ec1c1daa12113e6c
rafaelgov95 <rafaelgov95@gmail.com>
2017-06-03 02:11:01 -0400

    Update

--- myapp-front/src/app/componente/_services/authentication.service.ts
+++ myapp-front/src/app/componente/_services/authentication.service.ts
@@ -5,18 +5,51 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: Http) { }
 
-    login(username: string, password: string) {
-        console.log('teste');
-        return this.http.post('/api/autentica', JSON.stringify({ email: username, senha: password }))
+    constructor(private http: Http) { }
+    // private loginUrl = 'http://localhost:3000/autentica';
+
+    // login(username: string, password: string) {
+    //     console.log('teste');
+
+
+    //     let body = new URLSearchParams();
+    //     body.set('username', username);
+    //     body.set('password', password);
+    //     // return this.http.post('http://localhost:3000/autentica', JSON.stringify({ email: username, senha: password }))
+    //     return this.http.post(this.loginUrl, { 'username': username, 'password': password }, this.headers)
+
+    //         .map((response: Response) => {
+    //             console.log(JSON.stringify({ email: username, senha: password }));
+    //             // login successful if there's a jwt token in the response
+    //             let user = response.json();
+    //             console.log(user);
+    //             if (user && user.token) {
+    //                 // store user details and jwt token in local storage to keep user logged in between page refreshes
+    //                 localStorage.setItem('currentUser', JSON.stringify(user));
+    //             }
+    //         });
+    // }
+    login(username, password) {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        let urlSearchParams = new URLSearchParams();
+        urlSearchParams.append('email', username);
+        urlSearchParams.append('senha', password);
+        let body = urlSearchParams.toString()
+        return this.http.post('http://localhost:3000/autentica', body, { headers: headers })
             .map((response: Response) => {
-                      console.log('teste');
                 // login successful if there's a jwt token in the response
-                let user = response.json();
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                let body = response.json();
+                if (body) {
+                    if (body.user && body.user.accessToken) {
+                        // store user details and jwt token in local storage to keep user logged in between page refreshes
+                        localStorage.setItem('currentUser', JSON.stringify(body));
+                    }
+                }
+                else {
+                    console.log("erro");
+                    return body;
                 }
             });
     }
--- myapp-front/src/app/componente/home/home.component.ts
+++ myapp-front/src/app/componente/home/home.component.ts
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
 
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        console.log(this.currentUser);
     }
 
     ngOnInit() {
